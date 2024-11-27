f80191a3a2915c44b392ddeb1f4741e69a29d91d
fasta <fastakovskiy.mihail04@gmail.com>
2024-11-27 22:47:39 +0200

    Добавьте файлы проекта.

---
+++ NetCsharpHouseBuilding/House.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetCsharpHouseBuilding
+{
+    interface IPart
+    {
+        string Name { get; set; }
+        bool isBuilt { get; set; }
+        void Build();
+    }
+    interface IWorker
+    {
+        void Work(House house);
+    }
+    class House
+    {
+        public List<IPart> Parts { get; set; }
+        public bool IsComplete => Parts.TrueForAll(part => part.isBuilt);
+        public House(IEnumerable<IPart> parts)
+        {
+            Parts = new List<IPart>(parts);
+        }
+
+    }
+
+    abstract class PartBase : IPart
+    {
+        public string Name { get; set; }
+        public bool isBuilt { get; set; }
+
+        protected PartBase(string name)
+        {
+            Name = name;
+        }
+        public virtual void Build()
+        {
+            isBuilt = true;
+            Console.WriteLine($"{Name} was built");
+        }
+    }
+
+
+    class Basement : PartBase
+    {
+        public Basement() : base("Basement") { }
+    }
+    class Walls : PartBase
+    {
+        public Walls() : base("Wall") { }
+    }
+    class Door : PartBase
+    {
+        public Door() : base("Door") { }
+    }
+    class Window : PartBase
+    {
+        public Window() : base("Window") { }
+    }
+    class Roof : PartBase
+    {
+        public Roof() : base("Roof") { }
+    }
+    class Worker : IWorker
+    {
+        public void Work(House house)
+        {
+            foreach (var part in house.Parts)
+            {
+                if (!part.isBuilt)
+                {
+                    part.Build();
+                    return;
+                }
+            }
+        }
+    }
+    class TeamLeader : IWorker
+    {
+        public void Work(House house)
+        {
+            Console.WriteLine("Team Leader Report:");
+            foreach (var part in house.Parts)
+            {
+                Console.WriteLine($"{part.Name}: {(part.isBuilt ? "Built" : "Not Built")}");
+            }
+        }
+    }
+    class Team
+    {
+        public List<IWorker> Workers { get; set; }
+        public Team(IEnumerable<IWorker> workers)
+        {
+            Workers = new List<IWorker>(workers);
+        }
+        public void BuildHouse(House house)
+        {
+            while (!house.IsComplete)
+            {
+                foreach (var worker in Workers)
+                {
+                    if (house.IsComplete) break;
+                    worker.Work(house);
+                }
+            }
+
+            Console.WriteLine("The house is complete!");
+            DrawHouse();
+        }
+        private void DrawHouse()
+        {
+            Console.WriteLine("   ^   ");
+            Console.WriteLine("  / \\  ");
+            Console.WriteLine(" /   \\ ");
+            Console.WriteLine("/_____\\");
+            Console.WriteLine("|  _  |");
+            Console.WriteLine("| | | |");
+            Console.WriteLine("|_|_|_|");
+        }
+
+
+    }
+}
---
+++ NetCsharpHouseBuilding/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using NetCsharpHouseBuilding;
+
+Basement basement = new Basement();
+Walls wall1 = new Walls();
+Walls wall2 = new Walls();
+Walls wall3 = new Walls();
+Walls wall4 = new Walls();
+Door door = new Door();
+Window window1 = new Window();
+Window window2 = new Window();
+Window window3 = new Window();
+Window window4 = new Window();
+Roof roof = new Roof();
+
+House house=new House(new List<IPart> { basement, wall1, wall2, wall3, wall4, door, window1, window2, window3, window4, roof });
+
+
+Worker worker1 = new Worker();
+Worker worker2 = new Worker();
+TeamLeader teamLeader = new TeamLeader();
+
+Team team = new Team(new List<IWorker> { worker1, worker2, teamLeader });
+
+team.BuildHouse(house);
